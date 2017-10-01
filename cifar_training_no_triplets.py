6536fed57c856c1f26f704e8a36706ca0329e0a0
abby <sw62190>
2017-10-01 15:05:36 -0500

    Change logging, learning rate

--- cifar_training_no_triplets.py
+++ cifar_training_no_triplets.py
@@ -21,30 +21,34 @@ from tensorflow.contrib.slim.python.slim.nets import alexnet
 def main():
     ckpt_dir = './output/cifar/no_triplets/ckpts'
     log_dir = './output/cifar/no_triplets/logs'
-    filename = './inputs/cifar/train.txt'
+    train_log_file = open(os.path.join(log_dir,str(datetime.now())+'_train.txt'),'a')
+    test_log_file = open(os.path.join(log_dir,str(datetime.now())+'_test.txt'),'a')
+    train_filename = './inputs/cifar/train.txt'
+    test_filename = './inputs/cifar/test.txt'
     mean_file = './models/cifar/cifar_mean_im.npy'
     pretrained_net = None
     img_size = [256, 256]
     crop_size = [224, 224]
     num_iters = 200000
     summary_iters = 10
-    save_iters = 5000
-    learning_rate = .0001
+    save_iters = 1000
+    learning_rate = .00005
     margin = 10
     featLayer = 'alexnet_v2/fc8'
 
     batch_size = 100
     num_pos_examples = batch_size/10
 
-    # Queuing op loads data into input tensor
+    # Queuing op loads train_data into input tensor
     image_batch = tf.placeholder(tf.float32, shape=[batch_size, crop_size[0], crop_size[0], 3])
     label_batch = tf.placeholder(tf.int32, shape=(batch_size))
 
-    # Create data "batcher"
-    data = NonTripletSet(filename, mean_file, img_size, crop_size, batch_size, num_pos_examples)
+    # Create train_data "batcher"
+    train_data = NonTripletSet(train_filename, mean_file, img_size, crop_size, batch_size, num_pos_examples)
+    test_data = NonTripletSet(test_filename, mean_file, img_size, crop_size, batch_size, num_pos_examples)
 
     # after we've doctored everything, we need to remember to subtract off the mean
-    repMeanIm = np.tile(np.expand_dims(data.meanImage,0),[batch_size,1,1,1])
+    repMeanIm = np.tile(np.expand_dims(train_data.meanImage,0),[batch_size,1,1,1])
     final_batch = tf.subtract(image_batch,repMeanIm)
 
     print("Preparing network...")
@@ -61,7 +65,7 @@ def main():
     init_op = tf.global_variables_initializer()
 
     # Create a saver for writing training checkpoints.
-    saver = tf.train.Saver(max_to_keep=20)
+    saver = tf.train.Saver(max_to_keep=100)
 
     c = tf.ConfigProto()
     c.gpu_options.visible_device_list="0,1"
@@ -79,17 +83,26 @@ def main():
     ctr  = 0
     for step in range(num_iters):
         start_time1 = time.time()
-        batch, labels, ims = data.getBatch()
-        end_time1 = time.time()
-        start_time2 = time.time()
+        batch, labels, ims = train_data.getBatch()
         _, loss_val = sess.run([train_op, loss], feed_dict={image_batch: batch, label_batch: labels})
-        aa2 = sess.run(aa, feed_dict={image_batch: batch, label_batch: labels})
+        prediction=tf.argmax(feat,1)
+        train_best = sess.run([prediction],feed_dict={image_batch: batch, label_batch: labels})
+        train_accuracy = int(100*float(len(np.where(train_best==labels)[0]))/float(batch_size))
         end_time2 = time.time()
-
         duration = end_time2-start_time1
-
-        # if step % summary_iters == 0:
-        print('Step %d: loss = %.2f (%.3f sec), top1: %3f' % (step, loss_val, duration,float(len(np.where(aa2==0)[0]))/float(batch_size)))
+        if step % 50 == 0:
+            test_batch, test_labels, test_ims = test_data.getBatch()
+            test_best = sess.run([prediction],feed_dict={image_batch: test_batch, label_batch: test_labels})
+            test_accuracy = int(100*float(len(np.where(test_best==test_labels)[0]))/float(batch_size))
+            end_time2 = time.time()
+            duration = end_time2-start_time1
+            # if step % summary_iters == 0:
+            out_str = 'TEST: Step %d: top1-accuracy: %d' % (step,test_accuracy)
+            print(out_str)
+            test_log_file.write(out_str+'\n')
+        out_str = 'Step %d: loss = %.2f (%.3f sec), top1-accuracy: %d' % (step, loss_val, duration,train_accuracy)
+        print(out_str)
+        train_log_file.write(out_str+'\n')
         # Update the events file.
         # summary_str = sess.run(summary_op)
         # writer.add_summary(summary_str, step)
@@ -102,6 +115,8 @@ def main():
             saver.save(sess, pretrained_net, global_step=step)
 
     sess.close()
+    train_log_file.close()
+    test_log_file.close()
 
 if __name__ == "__main__":
     main()
