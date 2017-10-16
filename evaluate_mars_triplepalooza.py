8f8251a13adcb9b4e59ca786a41ac63b8efc239e
abby <sw62190>
2017-10-16 14:40:43 -0500

    Eval mars correctly

--- evaluate_mars_triplepalooza.py
+++ evaluate_mars_triplepalooza.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from classfile import NonTripletSet
+from classfile import MarsNonTripletSet
 import os
 import time
 import numpy as np
@@ -20,7 +20,7 @@ def getDist(feat,otherFeats):
 
 train_file = './inputs/mars/train.txt'
 test_file = './inputs/mars/test.txt'
-eval_net = './output/mars/ckpts/final-_lr0pt0001_outputSz100_margin0.3-19999'
+eval_net = './output/mars/ckpts/checkpoint-_lr0pt0001_outputSz100_margin0.3-4499'
 img_size = [256, 256]
 crop_size = [224, 224]
 featLayer = 'resnet_v2_50/logits'
@@ -30,13 +30,12 @@ batch_size = 100
 num_pos_examples = batch_size/10
 
 # Create train_data "batcher"
-train_data = NonTripletSet(train_file, mean_file, img_size, crop_size, batch_size, num_pos_examples)
-test_data = NonTripletSet(test_file, mean_file, img_size, crop_size, batch_size, num_pos_examples)
+train_data = MarsNonTripletSet(train_file, mean_file, img_size, crop_size, batch_size, num_pos_examples)
+test_data = MarsNonTripletSet(test_file, mean_file, img_size, crop_size, batch_size, num_pos_examples)
 
 image_batch = tf.placeholder(tf.float32, shape=[batch_size, crop_size[0], crop_size[0], 3])
 repMeanIm = np.tile(np.expand_dims(train_data.meanImage,0),[batch_size,1,1,1])
-noise = tf.random_normal(shape=[batch_size, crop_size[0], crop_size[0], 3], mean=0.0, stddev=3, dtype=tf.float32)
-final_batch = tf.add(tf.subtract(image_batch,repMeanIm),noise)
+final_batch = tf.subtract(image_batch,repMeanIm)
 label_batch = tf.placeholder(tf.int32, shape=(batch_size))
 
 print("Preparing network...")
@@ -60,11 +59,12 @@ numTrainingIms = len(trainingImsAndLabels)
 trainingFeats = np.empty((numTrainingIms,feat.shape[1]),dtype=np.float32)
 trainingIms = np.empty((numTrainingIms),dtype=object)
 trainingLabels = np.empty((numTrainingIms),dtype=np.int32)
+trainingCams = np.empty((numTrainingIms),dtype=np.int32)
 num_iters = numTrainingIms / batch_size
 
 print 'Computing training set features...'
 for step in range(0,num_iters):
-    # print step, '/', num_iters
+    print step, '/', num_iters
     if step == num_iters:
         end_ind = numTrainingIms
     else:
@@ -75,7 +75,8 @@ for step in range(0,num_iters):
     trainingIms[step*batch_size:end_ind] = ims
     labels = [i[1] for i in il]
     trainingLabels[step*batch_size:end_ind] = labels
-    batch = train_data.getBatchFromImageList(ims)
+    batch, cams = train_data.getBatchFromImageList(ims)
+    trainingCams[step*batch_size:end_ind] = cams
 
     while len(labels) < batch_size:
         labels += [labels[-1]]
@@ -89,14 +90,17 @@ trainingAccuracy = np.zeros((numTrainingIms,100))
 for idx in range(numTrainingIms):
     thisFeat = trainingFeats[idx,:]
     thisLabel = trainingLabels[idx]
+    thisCam = trainingCams[idx]
+    sameCamInds = np.where(trainingCams==thisCam)[0]
     dists = getDist(thisFeat,trainingFeats)
-    sortedInds = np.argsort(dists)[1:]
+    dists[sameCamInds] = 100000000000
+    sortedInds = np.argsort(dists)
     sortedLabels = trainingLabels[sortedInds][:100]
     if thisLabel in sortedLabels:
         topHit = np.where(sortedLabels==thisLabel)[0][0]
         trainingAccuracy[idx,topHit:] = 1
-    # if idx%10==0:
-    #     print idx,': ',np.mean(trainingAccuracy[:idx,:],axis=0)[0]
+    if idx%10==0:
+        print idx,': ',np.mean(trainingAccuracy[:idx,:],axis=0)[0]
 
 sess.close()
 
@@ -112,11 +116,12 @@ numTestingIms = len(testingImsAndLabels)
 testingFeats = np.empty((numTestingIms,feat.shape[1]),dtype=np.float32)
 testingIms = np.empty((numTestingIms),dtype=object)
 testingLabels = np.empty((numTestingIms),dtype=np.int32)
+testingCams = np.empty((numTestingIms),dtype=np.int32)
 num_iters = numTestingIms / batch_size
 
 print 'Computing testing set features...'
 for step in range(0,num_iters):
-    # print step, '/', num_iters
+    print step, '/', num_iters
     if step == num_iters:
         end_ind = numTestingIms
     else:
@@ -127,7 +132,8 @@ for step in range(0,num_iters):
     testingIms[step*batch_size:end_ind] = ims
     labels = [i[1] for i in il]
     testingLabels[step*batch_size:end_ind] = labels
-    batch = test_data.getBatchFromImageList(ims)
+    batch, cams = test_data.getBatchFromImageList(ims)
+    testingCams[step*batch_size:end_ind] = cams
 
     while len(labels) < batch_size:
         labels += [labels[-1]]
@@ -141,14 +147,17 @@ testingAccuracy = np.zeros((numTestingIms,100))
 for idx in range(numTestingIms):
     thisFeat = testingFeats[idx,:]
     thisLabel = testingLabels[idx]
+    thisCam = testingCams[idx]
+    sameCamInds = np.where(testingCams==thisCam)[0]
     dists = getDist(thisFeat,testingFeats)
-    sortedInds = np.argsort(dists)[1:]
+    dists[sameCamInds] = 100000000000
+    sortedInds = np.argsort(dists)
     sortedLabels = testingLabels[sortedInds][:100]
     if thisLabel in sortedLabels:
         topHit = np.where(sortedLabels==thisLabel)[0][0]
         testingAccuracy[idx,topHit:] = 1
-    # if idx%10==0:
-    #     print idx,': ',np.mean(testingAccuracy[:idx,:],axis=0)[0]
+    if idx%10==0:
+        print idx,': ',np.mean(testingAccuracy[:idx,:],axis=0)[0]
 
 sess.close()
 
