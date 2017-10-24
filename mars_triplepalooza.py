a81458941824e85bf65a3d5d04bf6b8dc4a2fc34
abby <sw62190>
2017-10-24 11:32:15 -0500

    sparsity constraint

--- mars_triplepalooza.py
+++ mars_triplepalooza.py
@@ -156,7 +156,8 @@ def main(margin,output_size,learning_rate,is_overfitting):
     loss1 = tf.reduce_mean(tf.maximum(0.,tf.multiply(mask,margin + posDistsRep - allDists)))
     loss2 = tf.reduce_mean(tf.reduce_sum(tf.abs(weights),axis=1))
 
-    loss = loss1 + loss2
+    lmbd = .001
+    loss = loss1 + lmbd*loss2
 
     # loss = tf.log(tf.reduce_sum(tf.exp(posDistsRep))) - tf.log(tf.reduce_sum(tf.exp(margin - allDists)))
 
