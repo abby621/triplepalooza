2e246324c34815e01c6f93e5702d663e02eeea58
abby <sw62190>
2017-10-24 11:34:25 -0500

    sparsity constraint

--- mars_triplepalooza.py
+++ mars_triplepalooza.py
@@ -187,13 +187,15 @@ def main(margin,output_size,learning_rate,is_overfitting):
     for step in range(num_iters):
         start_time = time.time()
         batch, labels, cams, ims = train_data.getBatch()
-        _, loss_val,ls1,ls2 = sess.run([train_op, loss,loss1,loss2], feed_dict={image_batch: batch, label_batch: labels, camera_batch: cams})
+        _, loss_val,ls1,ls2,wgts = sess.run([train_op, loss,loss1,loss2,weights], feed_dict={image_batch: batch, label_batch: labels, camera_batch: cams})
         end_time = time.time()
         duration = end_time-start_time
         if step % summary_iters == 0:
             out_str = 'Step %d: loss = %.6f (loss1: %.6f | loss2: %.6f) (%.3f sec)' % (step, loss_val,ls1,ls2,duration)
             print(out_str)
             train_log_file.write(out_str+'\n')
+        if step % 100 == 0:
+            print wgts[0]
         # Update the events file.
         # summary_str = sess.run(summary_op)
         # writer.add_summary(summary_str, step)
