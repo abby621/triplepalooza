195348378186855ffc415e69bc5e2250df5a2f64
abby <sw62190>
2017-11-28 03:24:33 -0600

    rotate and crop

--- tc_triplepalooza.py
+++ tc_triplepalooza.py
@@ -39,7 +39,7 @@ def main(margin,batch_size,output_size,learning_rate,is_overfitting):
     log_dir = './output/traffickcam/logs'
     train_filename = './inputs/traffickcam/train_equal_no_duplicates.txt'
     mean_file = './models/traffickcam/tc_mean_im.npy'
-    pretrained_net = os.path.join(ckpt_dir,'checkpoint-201711221229_lr0pt0001_outputSz128_margin0pt3-143908
+    pretrained_net = os.path.join(ckpt_dir,'checkpoint-201711221229_lr0pt0001_outputSz128_margin0pt3-143908'
     crop_size = [224, 224]
     num_iters = 200000
     summary_iters = 10
