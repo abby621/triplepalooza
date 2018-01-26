ba27a3e0448ecb7d0738213578568df960c2c608
abby <sw62190>
2018-01-26 15:57:01 -0600

    change fine tuning network

--- tc_triplepalooza.py
+++ tc_triplepalooza.py
@@ -2,9 +2,9 @@
 """
 # python mars_triplepalooza.py margin output_size learning_rate is_overfitting
 # If overfitting:
-# python tc_triplepalooza.py .3 50 128 .0001 True
+# python tc_triplepalooza.py .3 50 128 .0001 True '2'
 # Else:
-# python tc_triplepalooza.py .3 120 128 .0001 False
+# python tc_triplepalooza.py .3 120 128 .0001 False '3'
 """
 
 import tensorflow as tf
@@ -39,7 +39,7 @@ def main(margin,batch_size,output_size,learning_rate,is_overfitting,whichGPU):
     log_dir = './output/traffickcam/logs'
     train_filename = './inputs/traffickcam/train_equal_no_duplicates.txt'
     mean_file = './models/traffickcam/tc_mean_im.npy'
-    pretrained_net = os.path.join(ckpt_dir,'checkpoint-201801091419_lr0pt0001_outputSz128_margin0pt3-14456')
+    pretrained_net = os.path.join(ckpt_dir,'checkpoint-201801111434_lr1e-05_outputSz128_margin0pt3-199999')
     # pretrained_net = None
     img_size = [256, 256]
     crop_size = [224, 224]
