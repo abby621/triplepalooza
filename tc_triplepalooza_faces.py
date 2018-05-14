af3e9aff57e51b0bd87ffd27691fdd11e51d5d86
abby <sw62190>
2018-05-14 09:38:17 -0500

    param list wrong on faces

--- tc_triplepalooza_faces.py
+++ tc_triplepalooza_faces.py
@@ -45,11 +45,6 @@ def main(margin,batch_size,output_size,learning_rate,whichGPU):
     featLayer = 'resnet_v2_50/logits'
 
     is_training = True
-    if is_overfitting.lower()=='true':
-        is_overfitting = True
-    else:
-        is_overfitting = False
-
     margin = float(margin)
     batch_size = int(batch_size)
     output_size = int(output_size)
