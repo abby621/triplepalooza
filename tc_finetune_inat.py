7425ed966b28f5c35e52307cc5cfba013c770105
Stylianou <abby.halley@gmail.com>
2018-05-31 19:43:50 -0500

    batch size changes

--- tc_finetune_inat.py
+++ tc_finetune_inat.py
@@ -6,7 +6,7 @@
 """
 
 import tensorflow as tf
-from classfile import CombinatorialTripletSet
+from classfile import iNatCombinatorialTripletSet
 import os.path
 import time
 from datetime import datetime
@@ -39,8 +39,8 @@ def main(margin,batch_size,output_size,learning_rate,whichGPU,is_finetuning,pret
     train_filename = './inputs/inat/train.txt'
     mean_file = './models/inat/meanIm.npy'
 
-    img_size = [256, 256]
-    crop_size = [224, 224]
+    img_size = [525, 525]
+    crop_size = [512, 512]
     num_iters = 200000
     summary_iters = 10
     save_iters = 500
@@ -53,14 +53,14 @@ def main(margin,batch_size,output_size,learning_rate,whichGPU,is_finetuning,pret
     output_size = int(output_size)
     learning_rate = float(learning_rate)
 
-    if batch_size%30 != 0:
-        print 'Batch size must be divisible by 30!'
+    if batch_size%4 != 0:
+        print 'Batch size must be divisible by 4!'
         sys.exit(0)
 
-    num_pos_examples = batch_size/30
+    num_pos_examples = batch_size/4
 
     # Create data "batcher"
-    train_data = CombinatorialTripletSet(train_filename, mean_file, img_size, crop_size, batch_size, num_pos_examples, isTraining=is_training)
+    train_data = iNatCombinatorialTripletSet(train_filename, mean_file, img_size, crop_size, batch_size, num_pos_examples, isTraining=is_training)
     numClasses = len(train_data.files)
     numIms = np.sum([len(train_data.files[idx]) for idx in range(0,numClasses)])
     datestr = datetime.now().strftime("%Y_%m_%d_%H%M")
